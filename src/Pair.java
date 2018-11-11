fefe4ff1ffd47fe0413a9263fbca86c253565eb6
Glenn Skelton <skeltong2@gmail.com>
2018-11-11 14:16:08 -0500

    Finished commenting Pair class

--- src/Pair.java
+++ src/Pair.java
@@ -10,6 +10,7 @@ public class Pair {
     private String partnerB;
     private int total;
 
+    /*********************** CONSTRUCTORS **********************/
     public Pair() {
         this.partnerA = null;
         this.partnerB = null;
@@ -37,14 +38,27 @@ public class Pair {
 
     /********************** SETTERS/GETTERS ***********************/
 
+    /**
+     * Purpose: To retrieve partnerA.
+     * @return String - the name of the person stored in partnerA.
+     */
     public String getPartnerA() {
         return this.partnerA == null ? "" : new String(this.partnerA);
     }
 
+    /**
+     * Purpose: To retrieve partnerB.
+     * @return String - the name of the person stored in partnerB.
+     */
     public String getPartnerB() {
         return this.partnerB == null ? "" : new String(this.partnerB);
     }
 
+    /**
+     * Purpose: To set partnerA to the name passed in. If the name
+     * passed in is an empty string, partnerA remains null.
+     * @return void
+     */
     public void setPartnerA(String partnerA) {
         if (partnerA.length() > 0) {
             this.partnerA = new String(partnerA);
@@ -52,6 +66,11 @@ public class Pair {
         }
     }
 
+    /**
+     * Purpose: To set partnerB to the name passed in. If the name
+     * passed in is an empty string, partnerB remains null.
+     * @return void
+     */
     public void setPartnerB(String partnerB) {
         if (partnerB.length() > 0) {
             this.partnerB = new String(partnerB);
@@ -60,28 +79,42 @@ public class Pair {
     }
 
     /**
-     *
+     * Purpose: To retrieve the name of the person paired with the
+     * name of the person passed in.
+     * @return String - partnerB paired with partnerA
      */
     public String getPair(String name) {
-        if (this.getPartnerA().toUpperCase().equals(name.toUpperCase())) return this.getPartnerB();
+        if (this.getPartnerA().toUpperCase().equals(name.toUpperCase()))
+            return this.getPartnerB();
         else return null;
     }
 
     /**
      * Purpose: Returns true if the pairs have a name in common
      * else false.
+     * @return Boolean - true if pair passed in matches exisiting couples
+     * pairing else false
      */
     public Boolean compare(Pair pair) {
         Boolean comp =
-                    (this.getPartnerA().equals(pair.getPartnerA()) && this.getPartnerB().equals(pair.getPartnerB())) ||
-                    (this.getPartnerA().equals(pair.getPartnerB()) && this.getPartnerB().equals(pair.getPartnerA()));
+            (this.getPartnerA().equals(pair.getPartnerA()) &&
+                this.getPartnerB().equals(pair.getPartnerB())) ||
+            (this.getPartnerA().equals(pair.getPartnerB()) &&
+                this.getPartnerB().equals(pair.getPartnerA()));
         return comp; //
     }
 
+    /**
+     * Purpose: To retrieve the number of people stored in a pairing.
+     */
     public int size() {
         return this.total;
     }
 
+    /**
+     * Purpose: To return a string form of the Pair class
+     * @return String - (personA, PersonB)
+     */
     @Override
     public String toString() {
         return "(" + getPartnerA() + ", " + getPartnerB() + ")";
